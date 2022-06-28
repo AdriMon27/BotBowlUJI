09af44a785b7c3bd9a25f35ca3b41f2006e28649
AdriMon27 <90414103+AdriMon27@users.noreply.github.com>
2022-06-28 10:21:50 +0200

    codigo a github otra vez

--- botbowl/examples/bot_test_example.py
+++ botbowl/examples/bot_test_example.py
@@ -1,29 +1,31 @@
 #!/usr/bin/env python3
 from scripted_bot_example import *
+from random_bot_example import *
 
 import botbowl as botbowl
 import time as time
 
-config = botbowl.load_config("bot-bowl")
-config.competition_mode = False
-config.pathfinding_enabled = True
-config.debug_mode = False
-ruleset = botbowl.load_rule_set(config.ruleset, all_rules=False)  # We don't need all the rules
-arena = botbowl.load_arena(config.arena)
-home = botbowl.load_team_by_filename("human", ruleset)
-away = botbowl.load_team_by_filename("human", ruleset)
+if __name__ == '__main__':
+    config = botbowl.load_config("bot-bowl")
+    config.competition_mode = False
+    config.pathfinding_enabled = True
+    config.debug_mode = False
+    ruleset = botbowl.load_rule_set(config.ruleset, all_rules=False)  # We don't need all the rules
+    arena = botbowl.load_arena(config.arena)
+    home = botbowl.load_team_by_filename("human", ruleset)
+    away = botbowl.load_team_by_filename("human", ruleset)
 
-# Play 10 games
-for i in range(10):
-    home_agent = botbowl.make_bot('scripted')
-    home_agent.name = "Bot 1"
-    away_agent = botbowl.make_bot('scripted')
-    away_agent.name = "Bot 2"
-    game = botbowl.Game(i, home, away, home_agent, away_agent, config, arena=arena, ruleset=ruleset)
-    game.config.fast_mode = True
+    # Play 10 games
+    for i in range(1):
+        home_agent = botbowl.make_bot('first')
+        home_agent.name = "Bot 1"
+        away_agent = botbowl.make_bot('my-random-bot')
+        away_agent.name = "Bot 2"
+        game = botbowl.Game(i, home, away, home_agent, away_agent, config, arena=arena, ruleset=ruleset)
+        game.config.fast_mode = True
 
-    print("Starting game", (i+1))
-    start = time.time()
-    game.init()
-    end = time.time()
-    print(end - start)
+        print("Starting game", (i+1))
+        start = time.time()
+        game.init()
+        end = time.time()
+        print(end - start)
